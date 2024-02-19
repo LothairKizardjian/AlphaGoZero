70e59a5a631ea226a148d01f2d6a1ba95f0a7381
Admin <admin@MacBook-Pro-de-Admin.local>
2024-02-19 17:21:53 +0100

    added unit tests for GoGame

--- src/go_game.py
+++ src/go_game.py
@@ -1,25 +1,73 @@
 class GoGame:
     def __init__(self, size=19):
-        self.size = size
-        self.board = [[0 for _ in range(size)] for _ in range(size)]
-        self.current_player = 1
-        self.opponent = -1
-        self.pass_count = 0  # Track consecutive passes
-        self.last_action = None
+        self._size = size
+        self._board = [[0 for _ in range(size)] for _ in range(size)]
+        self._current_player = 1
+        self._opponent = -1
+        self._pass_count = 0  # Track consecutive passes
+        self._last_action = None
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, s):
+        self._size = s
+
+    @property
+    def board(self):
+        return self._board
+
+    @property
+    def current_player(self):
+        return self._current_player
+
+    @current_player.setter
+    def current_player(self, val):
+        self._current_player = val
+
+    @property
+    def opponent(self):
+        return self._opponent
+
+    @opponent.setter
+    def opponent(self, val):
+        self._opponent = val
+
+    @property
+    def pass_count(self):
+        return self._pass_count
+
+    @pass_count.setter
+    def pass_count(self, val):
+        self._pass_count = val
+
+    @property
+    def last_action(self):
+        return self._last_action
+
+    @last_action.setter
+    def last_action(self, val):
+        self._last_action = val
 
     def is_valid_move(self, row, col):
         return self.board[row][col] == 0
 
     def place_stone(self, row, col):
-        if not self.is_valid_move:
+        if not self.is_valid_move(row, col):
             return False
         self.board[row][col] = self.current_player
         self.current_player, self.opponent = self.opponent, self.current_player
         self.last_action = (row, col)  # Update last action
         return True
 
-    def get_last_action(self):
-        return self.last_action
+    def no_empty_positions(self):
+        for row in range(self.size):
+            for col in range(self.size):
+                if self.board[row][col] == 0:
+                    return False
+        return True
 
     def is_game_over(self):
         if self.pass_count >= 2:
@@ -28,18 +76,11 @@ class GoGame:
             return True  # No more empty positions on the board
         return False
 
-    def no_empty_positions(self):
-        for row in range(self.size):
-            for col in range(self.size):
-                if self.board[row][col] == " ":
-                    return False
-        return True
-
     def get_possible_actions(self):
         actions = []
         for row in range(self.size):
             for col in range(self.size):
-                if self.board[row][col] == " ":
+                if self.board[row][col] == 0:
                     actions.append((row, col))
         return actions
 
--- test/src/test_go_game.py
+++ test/src/test_go_game.py
@@ -15,8 +15,37 @@ class TestGoGame(unittest.TestCase):
         self.assertFalse(self.go_game.is_valid_move(0, 0))
 
     def test_place_stone_ok(self):
-        self.go_game.place_stone(0, 0)
+        self.assertTrue(self.go_game.place_stone(0, 0))
         self.assertEqual(self.go_game.board[0][0], 1)
+        self.assertEqual(self.go_game.current_player, -1)
+        self.assertEqual(self.go_game.opponent, 1)
+        self.assertEqual(self.go_game.last_action, (0, 0))
+
+    def test_place_stone_nok(self):
+        self.assertTrue(self.go_game.place_stone(0, 0))
+        self.assertFalse(self.go_game.place_stone(0, 0))
+
+    def test_no_empty_positions(self):
+        self.assertFalse(self.go_game.no_empty_positions())
+        for i in range(self.go_game.size):
+            for j in range(self.go_game.size):
+                self.go_game.place_stone(i, j)
+        self.assertTrue(self.go_game.no_empty_positions())
+
+    def test_is_game_over(self):
+        self.assertFalse(self.go_game.is_game_over())
+        self.go_game.pass_count = 2
+        self.assertTrue(self.go_game.is_game_over())
+        self.go_game.pass_count = 0
+        for i in range(self.go_game.size):
+            for j in range(self.go_game.size):
+                self.go_game.place_stone(i, j)
+        self.assertTrue(self.go_game.is_game_over())
+
+    def test_get_possible_actions(self):
+        for i in range(self.go_game.size):
+            for j in range(self.go_game.size):
+                self.assertTrue((i, j) in self.go_game.get_possible_actions())
 
 
 if __name__ == "__main__":
